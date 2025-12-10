8eae5920d2aff3d94794bf866bd7aca3d6569691
Dhanan <123396272+ThannooDhanan@users.noreply.github.com>
2025-12-09 22:24:12 -0500

    Movement based on camera.

--- Scripts/mover.gd
+++ Scripts/mover.gd
@@ -1,12 +1,66 @@
 extends CharacterBody3D
 
-@onready var rigidBody := $RigidBody3D
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouseSensitivity := .25
+@onready var cameraPivot := %"Camera Pivot"
+@onready var player_cam := %playerCam
 
-@export var speed := .4
+@export_group("Character Traits")
+@export var speed := 8.0
+@export var max_velocity : int
+@export var acceleration := 12.0
+@export var rotationSpeed := 10.0
+@export var jumpStrength := 10.0
 
+@onready var player_skin := %"SnowmanSkin"
+var gravity := -30
+var camera_input_direction := Vector2.ZERO
+var last_mov_direction := Vector3.FORWARD
 
-func _process(delta):
-	var direction := Vector3.ZERO
-	direction = Vector3(Input.get_axis("Left","Right"), 0, Input.get_axis("Forward","Backward"))
-	rigidBody.apply_impulse(direction.normalized() * speed)
-	pass
+func _input(event):
+	if (event.is_action_pressed("left_click")):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if (event.is_action_pressed("ui_cancel")):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func _unhandled_input(event):
+	var is_camera_motion := (
+		event is InputEventMouseMotion and 
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED)
+	if is_camera_motion:
+		camera_input_direction = event.screen_relative * mouseSensitivity
+
+func _physics_process(delta):
+	"""Setup for player movement"""
+	var direction := Vector2.ZERO
+	direction = Input.get_vector("Left","Right","Forward","Backward")
+	var forward : Vector3 = player_cam.global_basis.z
+	var right : Vector3 = player_cam.global_basis.x
+	var verticalVelocity := velocity.y
+	velocity.y = 0
+	var move_direction : Vector3 = forward * direction.y + right * direction.x
+	
+	"""Camera rotation via mouse motion"""
+	cameraPivot.rotation.x += camera_input_direction.y * delta
+	cameraPivot.rotation.x = clamp(cameraPivot.rotation.x, -PI/6.0, PI/3.0)
+	cameraPivot.rotation.y -= camera_input_direction.x * delta
+	
+	camera_input_direction = Vector2.ZERO
+	
+	"""Moves player"""
+	move_direction.y = 0.0
+	velocity = velocity.move_toward(move_direction.normalized() * speed, acceleration * delta)	
+	velocity.y = verticalVelocity + gravity * delta
+	velocity.y = clamp(velocity.y, -300, INF)
+	
+	"""Jumps"""
+	var is_jumping := Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_jumping:
+		velocity.y += jumpStrength
+	move_and_slide()
+	
+	"""Faces Player"""
+	if move_direction.length() > 0.2:
+		last_mov_direction = move_direction
+	var target_angle :=  Vector3.FORWARD.signed_angle_to(last_mov_direction, Vector3.UP)
+	player_skin.global_rotation.y = lerp_angle(player_skin.rotation.y, target_angle, rotationSpeed * delta)
